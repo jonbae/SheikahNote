bcf28a63f301ed355e158e9fdd4adc5696b5627a
jonbae <jonbae61596@gmail.com>
2019-12-02 21:33:55 -0500

    tag input functionality

--- frontend/components/main_page/notes/note_footer.jsx
+++ frontend/components/main_page/notes/note_footer.jsx
@@ -12,9 +12,27 @@ class NoteShowFooter extends React.Component {
     this.props.requestAllTags();
   }
 
+  onEnter(e) {
+    if (e.keyCode == 13 && e.shiftKey == false) {
+      this.props.createTag(this.state);
+      this.setState({ name: "" });
+    } else {
+      null;
+    }
+  }
+
   render() {
-    console.log(this.props.note);
-    return <input type="text" />;
+    // console.log(this.props.note);
+    return (
+      <div className="note-show-footer">
+        <input
+          type="text"
+          onKeyDown={this.onEnter}
+          onChange={e => this.setState({ name: e.target.value })}
+          value={this.state.name}
+        />
+      </div>
+    );
   }
 }
 
